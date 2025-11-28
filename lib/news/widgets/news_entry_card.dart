6c77c8fbba3f64fbc4e520397d02332452a98129
Farrel <farrelacc2@gmail.com>
2025-11-29 01:11:18 +0700

    Added news detail and share button

--- lib/news/widgets/news_entry_card.dart
+++ lib/news/widgets/news_entry_card.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:sporra_mobile/news/models/news_entry.dart';
